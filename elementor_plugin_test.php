92398fb683636531544425b813fddcd955e3bb06
Mahmud Hasan Imran <67129592+imran150@users.noreply.github.com>
2020-12-17 09:08:43 +0600

    Fix elementor elements

--- elementor_plugin_test.php
+++ elementor_plugin_test.php
@@ -1,118 +1,190 @@
 <?php
-/*
-Plugin Name:       My Basics Plugin
-Plugin URI:        https://example.com/plugins/the-basics/
-Description:       Handle the basics with this plugin.
-Version:           1.10.3
-Requires at least: 5.2
-Requires PHP:      7.2
-Author:            Imran
-Author URI:        https://author.example.com/
- License:           GPL v2 or later
-License URI:      elementortest
-Text Domain:       my-basics-plugin
- Domain Path:       /languages
+/**
+ * Plugin Name: Elementor Hello World
+ * Description: Elementor sample plugin.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.2.1
+ * Author:      Author Name
+ * Author URI:  https://elementor.com/
+ * Text Domain: elementor-hello-world
+ * Elementor tested up to: 3.0.0
+ * Elementor Pro tested up to: 3.0.0
  */
- 
-if(!define('ABSPATH')){
-    die(__("Direct Access is not allowed","elementortest"));
-}
-final class Elementor_Test_Extension {
 
-	const VERSION="1.0.1";
-	const MINIMUM_ELEMENTOR_VERSION="2.0.1";
-	const MINIMUM_PHP_VERSION="7.0";
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-	private static $_instance = null;
-	public static function instance() {
+/**
+ * Main Elementor Hello World Class
+ *
+ * The init class that runs the Hello World plugin.
+ * Intended To make sure that the plugin's minimum requirements are met.
+ *
+ * You should only modify the constants to match your plugin's needs.
+ *
+ * Any custom code should go inside Plugin Class in the plugin.php file.
+ * @since 1.2.0
+ */
+final class Elementor_Hello_World {
 
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.2.1
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.2.1';
 
-	}
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.2.0
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '3.0.0';
 
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.2.0
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
 	public function __construct() {
-        add_action( 'plugins_loaded', [ $this, 'on_plugins_loaded' ] );
-    }
+
+		// Load translation
+		add_action( 'init', array( $this, 'i18n' ) );
+
+		// Init Plugin
+		add_action( 'plugins_loaded', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function i18n() {
+		load_plugin_textdomain( 'elementor-hello-world' );
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Validates that Elementor is already loaded.
+	 * Checks for basic plugin requirements, if one check fail don't continue,
+	 * if all check have passed include the plugin class.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
 	public function init() {
-        load_plugin_textdomain( 'elementor-test-extension' );
-        // Check if Elementor installed and activated
+
+		// Check if Elementor installed and activated
 		if ( ! did_action( 'elementor/loaded' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
-			return false;
-        }
-        // Check for required Elementor version
+			add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
+			return;
+		}
+
+		// Check for required Elementor version
 		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
-			return false;
-        }     
-        // Check for required PHP version
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
+			return;
+		}
+
+		// Check for required PHP version
 		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
-			return false;
-        }
-        add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
-    }
-    public function init_widgets() {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
+			return;
+		}
 
-		// Include Widget files
-		require_once( __DIR__ . '/widgets/test-widget.php' );
-
-		// Register widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor_Test_Widget() );
-
+		// Once we get here, We have passed all validation checks so we can safely include our plugin
+		require_once( 'plugin.php' );
 	}
-    public function admin_notice_minimum_php_version() {
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
-
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementortest' ),
-			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortest' ) . '</strong>',
-			'<strong>' . esc_html__( 'PHP', 'elementortest' ) . '</strong>',
-			 self::MINIMUM_PHP_VERSION
-		);
-
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-
-	}
-    public function admin_notice_minimum_elementor_version() {
-
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
-
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementortest' ),
-			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortest' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementortest' ) . '</strong>',
-			 self::MINIMUM_ELEMENTOR_VERSION
-		);
-
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-
-	}
-    public function admin_notice_missing_main_plugin() {
-
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
 		$message = sprintf(
 			/* translators: 1: Plugin name 2: Elementor */
-			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementortest' ),
-			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementortest' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementortest' ) . '</strong>'
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-hello-world' ),
+			'<strong>' . esc_html__( 'Elementor Hello World', 'elementor-hello-world' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-hello-world' ) . '</strong>'
 		);
 
 		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-
 	}
 
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-	public function includes() {}
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-hello-world' ),
+			'<strong>' . esc_html__( 'Elementor Hello World', 'elementor-hello-world' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-hello-world' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
 
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-hello-world' ),
+			'<strong>' . esc_html__( 'Elementor Hello World', 'elementor-hello-world' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'elementor-hello-world' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+	}
 }
-Elementor_Test_Extension::instance();
 
- 
+// Instantiate Elementor_Hello_World.
+new Elementor_Hello_World();
